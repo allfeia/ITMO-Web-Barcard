4c60dac1e3f30fd7c2878ffbac9af6f0be9df9a3
Iara Shilova <yara.a.shilova@mail.ru>
2025-10-08 16:30:41 +0300

    Заккоментирован тест, чтобы снизить покрытие

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -14,14 +14,14 @@ describe("App component", () => {
     expect(button).toBeInTheDocument();
   });
 
-  it("должен увеличивать счётчик при клике", () => {
-    render(<App />);
+  // it("должен увеличивать счётчик при клике", () => {
+  //   render(<App />);
 
-    const button = screen.getByRole("button", { name: /count is 0/i });
-    fireEvent.click(button);
+  //   const button = screen.getByRole("button", { name: /count is 0/i });
+  //   fireEvent.click(button);
 
-    expect(
-      screen.getByRole("button", { name: /count is 1/i }),
-    ).toBeInTheDocument();
-  });
+  //   expect(
+  //     screen.getByRole("button", { name: /count is 1/i }),
+  //   ).toBeInTheDocument();
+  // });
 });

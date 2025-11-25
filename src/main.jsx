1fe4239ef976b031eaed3f757f842f4e44ded3be
Iara Shilova <yara.a.shilova@mail.ru>
2025-11-25 16:03:28 +0300

    минимальные изменения для взаимодествия фронта с бэком

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import StartPage from './StartPage.jsx'
 import SignInPage from "./sign-in-page/SignInPage.jsx";
 import Menu from "./Menu.jsx";
+import { AuthProvider } from './AuthContext.jsx'
 
 if (import.meta.env.MODE === 'development') {
     const { worker } = await import('./mocks/browser');
@@ -13,10 +14,12 @@ if (import.meta.env.MODE === 'development') {
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <AuthProvider>
       <Routes>
           <Route path="/" element={<StartPage />} />
           <Route path="/signInPage" element={<SignInPage />} />
           <Route path="/menu" element={<Menu />} />
       </Routes>
+    </AuthProvider>
   </BrowserRouter>
 )

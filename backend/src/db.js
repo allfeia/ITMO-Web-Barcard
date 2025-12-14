77fa6f0c0fc7b74f635bc1d201330c35970c96ea
allfeia <nabokova_alisa2005@mail.ru>
2025-12-14 20:45:29 +0300

    соединение к базе без прерываний при простое (исправлено)

--- backend/src/db.js
+++ backend/src/db.js
@@ -4,10 +4,11 @@ import "dotenv/config";
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   logging: false,
-  dialectOptions: {
-      ...((process.env.DATABASE_URL || "").includes("sslmode=require")
-          ? { ssl: { require: true, rejectUnauthorized: false } }
-          : {}),
+  dialectOptions: (process.env.DATABASE_URL || "").includes("sslmode=require")
+    ? {
+      ssl: { require: true, rejectUnauthorized: false },
       keepAlive: true,
-  },
+
+      }
+    : {},
 });

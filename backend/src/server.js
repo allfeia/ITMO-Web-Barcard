30d1e5cfd949e22712f91721660a87120f211149
allfeia <nabokova_alisa2005@mail.ru>
2025-12-15 02:14:25 +0300

    подключен багтрекинг sentry.io

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import helmet from "helmet";
 import cors from "cors";
 import rateLimit from "express-rate-limit";
 import "dotenv/config";
+import * as Sentry from "@sentry/node";
 
 import { sequelize } from "./db.js";
 import "./models.js";
@@ -10,14 +11,24 @@ import "./models.js";
 import authRoutes from "./routes/auth.js";
 import apiRoutes from "./routes/api.js";
 
+Sentry.init({
+    dsn: "https://4d49d2f7f9efb43e46b35dbe8cb28664@o4510535321255936.ingest.us.sentry.io/4510535581237248",
+    integrations: [
+        Sentry.httpIntegration(),
+        Sentry.expressIntegration(),
+    ],
+    tracesSampleRate: 0.1,
+    environment: process.env.NODE_ENV || "development",
+});
+
 const app = express();
 
 app.use(helmet());
 app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN?.split(",") || "*",
-    credentials: false,
-  }),
+    cors({
+        origin: process.env.CORS_ORIGIN?.split(",") || "*",
+        credentials: false,
+    }),
 );
 app.use(express.json({ limit: "1mb" }));
 
@@ -29,12 +40,13 @@ app.use("/api", apiRoutes);
 
 const port = process.env.PORT || 4000;
 (async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("DB connected");
-    app.listen(port, () => console.log(`API on http://localhost:${port}`));
-  } catch (e) {
-    console.error("DB error", e);
-    process.exit(1);
-  }
+    try {
+        await sequelize.authenticate();
+        console.log("DB connected");
+        app.listen(port, () => console.log(`API on http://localhost:${port}`));
+    } catch (e) {
+        console.error("DB error", e);
+        Sentry.captureException(e);
+        process.exit(1);
+    }
 })();

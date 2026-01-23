99c306df1bf52c67d1632e96caaf81da9805f692
Iara Shilova <yara.a.shilova@mail.ru>
2026-01-23 17:42:14 +0300

    hotfix

--- src/game-pages/Result.jsx
+++ src/game-pages/Result.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import OrderModal from "./OderCard.jsx";
 import { useAuth } from "../authContext/useAuth.js";
 
 function Result() {
   const { roles } = useAuth();
+  const cocktailId = useSelector((state) => state.game.cocktailId);
 
   const [orderOpen, setOrderOpen] = useState(false);
 
@@ -26,7 +28,7 @@ function Result() {
       <OrderModal
         open={orderOpen}
         onClose={() => setOrderOpen(false)}
-        cocktailId={2}
+        cocktailId={cocktailId}
       />
     </>
   );

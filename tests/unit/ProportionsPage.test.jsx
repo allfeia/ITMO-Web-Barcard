13440406080660a444d106ababb78d6327b47040
khizhmsh <mhiznicenko@gmail.com>
2026-01-23 05:00:03 +0300

    unit

--- tests/unit/ProportionsPage.test.jsx
+++ tests/unit/ProportionsPage.test.jsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import ProportionsPage from "../../src/game-pages/proportions-page/ProportionsPage";
 import * as redux from "react-redux";
